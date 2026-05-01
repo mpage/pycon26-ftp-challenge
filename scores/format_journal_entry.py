c9ddf5a09ee4e954694e82ecf7434aeda52a4b44
Matt Page <mpage@meta.com>
2026-05-01 15:33:32 -0700

    Wire up evaluation

---
+++ scores/format_journal_entry.py
@@ -0,0 +1,24 @@
+import argparse
+import json
+import time
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("github_username")
+    parser.add_argument("pr_url")
+    parser.add_argument("pr_number")
+    parser.add_argument("results_path")
+    args = parser.parse_args()
+
+    with open(args.results_path) as f:
+        results = json.load(f)
+
+    entry = {
+        "github_username": args.github_username,
+        "pr_url": args.pr_url,
+        "pr_number": args.pr_number,
+        "results": results,
+        "timestamp": int(time.time()),
+    }
+    print(json.dumps(entry))
