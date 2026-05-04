8f92ad35fdd119ea2724264db10cb0bd8107a07a
Matt Page <mpage@meta.com>
2026-05-04 14:27:40 -0700

    Tickle the build

--- submissions/mpage.py
+++ submissions/mpage.py
@@ -1,4 +1,4 @@
-"""Single-threaded reference implementation for the build graph simulator. 4"""
+"""Single-threaded reference implementation for the build graph simulator. 5"""
 
 from __future__ import annotations
 
