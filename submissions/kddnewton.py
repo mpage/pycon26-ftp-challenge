f14276c196ac4559402e453c39d8709deb826560
Kevin Newton <kddnewton@gmail.com>
2026-05-16 15:09:01 -0700

    Again

--- submissions/kddnewton.py
+++ submissions/kddnewton.py
@@ -1,98 +1,116 @@
-import _thread
+import ctypes
+import heapq
 import queue
 import threading
-import heapq
 
 from graph import BuildGraph
 
-NUM_WORKERS = 24
+
+_NUM_WORKERS = 24
+_NUM_WORKERS_M1 = _NUM_WORKERS - 1
+
+
+def immortalize(obj):
+    ctypes.c_uint32.from_address(id(obj) + 12).value = 0xFFFFFFFF
+
 
 _ready = queue.SimpleQueue()
 _lock = threading.Lock()
 _results = [None] * 20000
+_SENTINEL = object()
+
+immortalize(_ready)
+immortalize(_lock)
+immortalize(_results)
+immortalize(_SENTINEL)
+
+
+class _Remaining:
+    __slots__ = ("value",)
+    def __init__(self, value):
+        self.value = value
 
 
 def build_all(graph: BuildGraph):
     targets = graph.targets
-    remaining = len(targets)
-
-    in_degree = {name: len(target.deps) for name, target in targets.items()}
-    dependents = {name: [] for name in targets}
+    remaining = _Remaining(len(targets))
 
     for id, (name, target) in enumerate(targets.items()):
         target._id = id
-        if (deps := target.deps):
-            deps.sort(key=lambda d: d.name)
-            for dep in deps:
-                dependents[dep.name].append(name)
-        else:
-            _ready.put(name)
+        target._dependents = []
 
-    if _ready.qsize() > 1 or (max_fan_out := max((len(value) for value in dependents.values()), default=0)) > 1:
+    for target in targets.values():
+        if (deps := target.deps):
+            deps.sort(key=lambda dep: dep.name)
+            target._in_degree = len(deps)
+            for dep in deps:
+                dep._dependents.append(target)
+        else:
+            target._in_degree = 0
+            _ready.put(target)
+
+    if _ready.qsize() > 1 or (max_fan_out := max((len(target._dependents) for target in targets.values()), default=0)) > 1:
         heap = []
 
-        if max(in_degree.values()) > NUM_WORKERS and max_fan_out > NUM_WORKERS:
-            def worker():
-                nonlocal remaining
-                while True:
-                    if (name := _ready.get()) is None:
-                        return
+        if max(target._in_degree for target in targets.values()) > _NUM_WORKERS and max_fan_out > _NUM_WORKERS:
+            def worker(remaining):
+                target = _ready.get()
 
-                    while name is not None:
-                        target = targets[name]
-                        _results[target._id] = target.build({dep.name: _results[dep._id] for dep in target.deps})
+                while target is not _SENTINEL:
+                    _results[target._id] = target.build({dep.name: _results[dep._id] for dep in target.deps})
 
-                        next_name = None
-                        with _lock:
-                            remaining -= 1
-                            if remaining == 0:
-                                for _ in range(NUM_WORKERS - 1):
-                                    _ready.put(None)
-                                return
-                            for dependent in dependents[name]:
-                                in_degree[dependent] -= 1
-                                if in_degree[dependent] == 0:
-                                    heapq.heappush(heap, (-targets[dependent].work, dependent))
-                            if len(heap) == 1:
-                                _, next_name = heapq.heappop(heap)
+                    next_target = None
+                    with _lock:
+                        if remaining.value == 1:
+                            for _ in range(_NUM_WORKERS_M1):
+                                _ready.put(_SENTINEL)
+                            return
+
+                        remaining.value -= 1
+                        for dependent in target._dependents:
+                            if dependent._in_degree == 1:
+                                heapq.heappush(heap, (-dependent.work, dependent._id, dependent))
                             else:
-                                while heap:
-                                    _, ready_name = heapq.heappop(heap)
-                                    _ready.put(ready_name)
-                        name = next_name
-        else:
-            def worker():
-                nonlocal remaining
-                while True:
-                    if (name := _ready.get()) is None:
-                        return
+                                dependent._in_degree -= 1
 
-                    target = targets[name]
+                        if len(heap) == 1:
+                            next_target = heapq.heappop(heap)[2]
+                        else:
+                            while heap:
+                                _ready.put(heapq.heappop(heap)[2])
+
+                    if next_target is not None:
+                        target = next_target
+                    else:
+                        target = _ready.get()
+        else:
+            def worker(remaining):
+                while (target := _ready.get()) is not _SENTINEL:
                     _results[target._id] = target.build({dep.name: _results[dep._id] for dep in target.deps})
 
                     with _lock:
-                        remaining -= 1
-                        if remaining == 0:
-                            for _ in range(NUM_WORKERS - 1):
-                                _ready.put(None)
+                        if remaining.value == 1:
+                            for _ in range(_NUM_WORKERS_M1):
+                                _ready.put(_SENTINEL)
                             return
-                        for dependent in dependents[name]:
-                            in_degree[dependent] -= 1
-                            if in_degree[dependent] == 0:
-                                _ready.put(dependent)
 
-        for _ in range(NUM_WORKERS - 1):
-            _thread.start_joinable_thread(worker, daemon=True)
-        worker()
+                        remaining.value -= 1
+                        for dependent in target._dependents:
+                            if dependent._in_degree == 1:
+                                _ready.put(dependent)
+                            else:
+                                dependent._in_degree -= 1
+
+        for _ in range(_NUM_WORKERS_M1):
+            threading.Thread(target=worker, args=(remaining,), daemon=True).start()
+        worker(remaining)
     else:
-        name = _ready.get()
-        target = targets[name]
+        target = _ready.get()
 
         results = _results
         results[target._id] = target.build({})
 
-        for _ in range(remaining - 1):
-            dependent = dependents[name][0]
-            target = targets[dependent]
-            results[target._id] = target.build({name: results[targets[name]._id]})
-            name = dependent
+        for _ in range(remaining.value - 1):
+            dependent = target._dependents[0]
+            results[dependent._id] = dependent.build({target.name: results[target._id]})
+            target = dependent
