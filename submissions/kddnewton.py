505ab60191a85623b52c8bfb38e22232e8a26a60
Kevin Newton <kddnewton@gmail.com>
2026-05-16 11:12:00 -0700

    Again

--- submissions/kddnewton.py
+++ submissions/kddnewton.py
@@ -1,5 +1,6 @@
 import queue
 import threading
+import heapq
 
 from graph import BuildGraph
 
@@ -9,7 +10,10 @@ NUM_WORKERS = 24
 def build_all(graph: BuildGraph):
     targets = graph.targets
     remaining = len(targets)
+
+    heap = []
     ready = queue.SimpleQueue()
+
     in_degree = {name: len(target.deps) for name, target in targets.items()}
     dependents = {name: [] for name in targets}
     empty_deps = {}
@@ -19,12 +23,16 @@ def build_all(graph: BuildGraph):
             for dep in deps:
                 dependents[dep.name].append(name)
         else:
-            ready.put(name)
+            heapq.heappush(heap, (-target.work, name))
 
     results = {}
 
-    if ready.qsize() > 1 or any(len(value) > 1 for value in dependents.values()):
+    if len(heap) > 1 or any(len(v) > 1 for v in dependents.values()):
         lock = threading.Lock()
+
+        while heap:
+            _, name = heapq.heappop(heap)
+            ready.put(name)
 
         def worker():
             nonlocal remaining
@@ -44,16 +52,19 @@ def build_all(graph: BuildGraph):
                     for dependent in dependents[name]:
                         in_degree[dependent] -= 1
                         if in_degree[dependent] == 0:
-                            ready.put(dependent)
+                            heapq.heappush(heap, (-targets[dependent].work, dependent))
+                    while heap:
+                        _, ready_name = heapq.heappop(heap)
+                        ready.put(ready_name)
 
         threads = [threading.Thread(target=worker) for _ in range(NUM_WORKERS - 1)]
-        for t in threads:
-            t.start()
+        for thread in threads:
+            thread.start()
         worker()
-        for t in threads:
-            t.join()
+        for thread in threads:
+            thread.join()
     else:
-        name = ready.get()
+        _, name = heap[0]
         results[name] = targets[name].build({})
 
         for _ in range(remaining - 1):
